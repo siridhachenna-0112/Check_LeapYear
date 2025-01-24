aae622e1c2d74b388fdf95f374fa279af126a6e6
siridhachenna-0112 <chennasiridha5002@gmail.com>
2025-01-24 15:22:46 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear{
+    public static void main(String[] args) {
+        int year=2024;
+        if(((year%4==0)&&(year%100!=0))||(year%400==0))
+        {
+            System.out.println("leap year");
+        }else{
+            System.out.println("not a leap year");
+        }
+    }
+}
